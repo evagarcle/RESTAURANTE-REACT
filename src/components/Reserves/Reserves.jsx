11ecca37429470034d7be2a6fea0f0c6ae8257a4
Eva Garcia <evagarciaclemente96@gmail.com>
2024-05-24 07:50:28 +0200

    borrar prueba en allreserves

--- src/components/Reserves/Reserves.jsx
+++ src/components/Reserves/Reserves.jsx
@@ -2,15 +2,13 @@
 
 const Reserves = () => {
 
-  // const allReservations = JSON.parse(localStorage.getItem("reservationInfo")) || []
-
+ 
   return (
     <div>
       <h2>Todas las reservas</h2>
 
 
-      {/* {allReservations.map(reservation => <p key={reservation.name}>{reservation}</p> )} */}
-
+     
 
     </div>
   )
